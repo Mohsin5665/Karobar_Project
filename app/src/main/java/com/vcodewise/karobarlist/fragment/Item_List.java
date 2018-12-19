151c5a5eaf34c63fd053beadd189cae67633d26f
Mohsin Raza <goshi527@gmail.com>
2018-12-18 16:32:14 -0800

    final commit

--- app/src/main/java/com/vcodewise/karobarlist/fragment/Item_List.java
+++ app/src/main/java/com/vcodewise/karobarlist/fragment/Item_List.java
@@ -1,14 +1,32 @@
 package com.vcodewise.karobarlist.fragment;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.vcodewise.karobarlist.R;
+import com.vcodewise.karobarlist.models.ItemListing;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,11 +36,17 @@ import com.vcodewise.karobarlist.R;
  * Use the {@link Item_List#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Item_List extends Fragment {
+public class Item_List extends Fragment implements View.OnClickListener {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private RecyclerView recyclerView;
+    private static String URL_Web = "http://karobarlist.xtechnos.com/api/businesses";
+    private RecyclerView.Adapter adapter;
+    private List<ItemListing> itemlist;
+
+
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -68,6 +92,21 @@ public class Item_List extends Fragment {
         return inflater.inflate(R.layout.fragment_item__list, container, false);
     }
 
+
+    public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        recyclerView = (RecyclerView) view.findViewById(R.id.list_recycler);
+        recyclerView.setHasFixedSize(true);
+       // recyclerView.setLayoutManager(new LinearLayoutManager());
+
+        itemlist = new ArrayList<>();
+
+
+        loadRecyclerViewData();
+
+    }
+
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
@@ -92,6 +131,11 @@ public class Item_List extends Fragment {
         mListener = null;
     }
 
+    @Override
+    public void onClick(View view) {
+
+    }
+
     /**
      * This interface must be implemented by activities that contain this
      * fragment to allow an interaction in this fragment to be communicated
@@ -106,4 +150,58 @@ public class Item_List extends Fragment {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
+    private void loadRecyclerViewData() {
+
+        final ProgressDialog progressDialog = new ProgressDialog(getActivity());
+        progressDialog.setMessage("Loading Data...");
+        progressDialog.show();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URL_Web,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        progressDialog.dismiss();
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            JSONObject data = jsonObject.getJSONObject("data");
+                            JSONArray array = data.getJSONArray("businesses");
+
+                            for (int i = 0; i < array.length(); i++) {
+
+                                JSONObject o = array.getJSONObject(i);
+                                ItemListing list = new ItemListing(
+
+                                        o.getString("category"),
+                                        o.getString("address"),
+                                        o.getInt("review_ratings"),
+                                        o.getInt("total_reviews"),
+                                        o.getString("image")
+
+                                );
+                                itemlist.add(list);
+                            }
+
+                            adapter = new ItemListing_Adapter(itemlist,getContext());
+                        //    adapter = new ItemListing_Adapter(itemlist,Item_List.class);
+                            recyclerView.setAdapter(adapter);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+//                            Toast.makeText(Item_List.this, "Nothing is Available For Time Being", Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                progressDialog.dismiss();
+//                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getActivity().getApplicationContext());
+        requestQueue.add(stringRequest);
+    }
+
 }
